f6c5ccefe84b990a9543e1c547f21a4138daa8d5
Saif-Rahman666 <saf.saif@gmail.com>
2022-01-05 23:58:20 +0600

    dog

---
+++ components/voxel-dog.js
@@ -0,0 +1,4 @@
+import { useState, useEffect, useRef  , useCallback } from "react";
+import { Box, Spinner } from "@chakra-ui/react";
+import * as THREE from 'three'
+import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
