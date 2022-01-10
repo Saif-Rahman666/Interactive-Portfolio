b8175f4980a47a9a84f713e2968ba056a29d6b00
Saif-Rahman666 <saf.saif@gmail.com>
2022-01-11 02:02:54 +0600

    footprint used logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b432c7c18cf53332"; }
+/******/ 		__webpack_require__.h = function() { return "e455035cadc6e8ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
